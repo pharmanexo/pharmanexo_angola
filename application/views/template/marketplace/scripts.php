cd6f6e41cddb0add63ba1b07ade91ff62e365814
Felipe Lima <felipe.lima@pharmanexo.com.br>
2022-11-09 08:46:51 -0300

    script marketplace

--- application/views/template/marketplace/scripts.php
+++ application/views/template/marketplace/scripts.php
@@ -26,3 +26,91 @@ if (isset($scripts))
         echo "<script src='{$script}'></script>";
     }
 ?>
+<script>
+    $(function() {
+
+        <?php if (isset($this->session->id_sessao)) :
+            $tempo = $this->db->select("timestamp")->from('ci_sessions')->where("id= '{$this->session->id_sessao}'")->get()->row_array();
+        ?>
+            var verificaSessao = setInterval(tempo_sessao, 1000);
+            var alertar = 'true';
+            var tempoSessao = "<?php echo date('H:i:s', $tempo['timestamp'] + 300) ?>";
+            var tempoAlerta = "<?php echo date('H:i:s', $tempo['timestamp'] + 150) ?>";
+            var id = "<?php echo $this->session->id_sessao ?>";
+            var idUser = "<?php echo $this->session->id_usuario ?>";
+            var tempo = "<?php echo $tempo['timestamp'] ?>";
+
+
+            function tempo_sessao() {
+                var timeOut = new Date();
+                var hora = timeOut.toLocaleTimeString();
+                $("#tempo_sessao").html(hora);
+                if (tempoAlerta <= hora && alertar == 'true') {
+                    $('#modalAlerta').modal({
+                        backdrop: false
+                    })
+                    $("#myModal").modal({
+                        backdrop: true
+                    })
+                    alertar = 'false'
+                }
+                if (tempoSessao <= hora) {
+                    timeoutSessao()
+                    clearInterval(verificaSessao)
+                }
+            };
+
+            function timeoutSessao() {
+                $.ajax({
+                    type: "POST",
+                    url: "<?php echo base_url(); ?>/login/timeout_sessao",
+                    data: {
+                        id_usuario: idUser,
+                    },
+                    success: function(response) {
+                        console.log(response)
+                        if (response.type === 'error') {
+                            formWarning(response);
+                            localStorage.setItem("alertaSessao", true)
+                            window.location.replace('<?php echo base_url('login'); ?>');
+                        } else {
+                            formWarning(response)
+                        }
+                    },
+                });
+            }
+            if (localStorage.getItem("alertaSessao")) {
+                $("#alertaSessao").attr("hidden", false);
+                localStorage.clear();
+            }
+
+            $('#renovarSessao').click(function(e) {
+                $.ajax({
+                    type: "POST",
+                    url: "<?php echo base_url(); ?>/login/renovar_sessao",
+                    data: {
+                        id: id,
+                        timestamp: tempo
+                    },
+                    success: function(response) {
+                        console.log(response)
+                        if (response.type === 'success') {
+                            formWarning(response);
+                            window.location.reload(true);
+                        } else {
+                            formWarning(response)
+                        }
+                    }
+                });
+
+            });
+
+        <?php endif; ?>
+
+    });
+
+
+    $(document).ajaxStart(function() {
+        Pace.restart();
+    });
+</script>
