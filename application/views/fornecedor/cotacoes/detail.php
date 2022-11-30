83dabdf3087e0ea1f2d707d831e783586cd7a696
Felipe Lima <felipe.lima@pharmanexo.com.br>
2022-11-30 20:49:30 -0300

    pesquisa DePara

--- application/views/fornecedor/cotacoes/detail.php
+++ application/views/fornecedor/cotacoes/detail.php
@@ -292,7 +292,7 @@
                 $('#data-tableDePara<?php echo $k; ?>_filter input').addClass('searchInput<?php echo $k; ?>');
                 $('#fazerDePara<?php echo $k; ?>').on('click', function() {
                     $('.searchInput<?php echo $k; ?>').val(busca[0]);
-                    $('#data-tableDePara<?php echo $k; ?>').DataTable().draw();
+                    console.log($('#data-tableDePara<?php echo $k; ?>').DataTable().draw());
                 });
 
                 $('#btnCombinar<?php echo $k; ?>').on('click', function(e) {
