2a200be626e594bf9d79d7a86537df89bb80c1a9
Marlon Boecker <marlon.mbes@gmail.com>
2022-12-01 08:41:09 -0300

    remove sessao

--- application/views/template/scripts_bkp.php
+++ application/views/template/scripts_bkp.php
@@ -70,6 +70,123 @@ if (isset($scripts))
 <script>
     $(function() {
 
+        <?php if (isset($this->session->id_sessao)) :
+            $tempo = $this->db->select("timestamp")->from('ci_sessions')->where("id= '{$this->session->id_sessao}'")->get()->row_array();
+        ?>
+            /*var verificaSessao = setInterval(tempo_sessao, 1000);
+            var alertar = 'true';
+            var tempoSessao = "<?php echo date('H:i:s', $tempo['timestamp'] + 3600) ?>";
+            var tempoAlerta = "<?php echo date('H:i:s', $tempo['timestamp'] + 3000) ?>";
+            var id = "<?php echo $this->session->id_sessao ?>";
+            var idUser = "<?php echo $this->session->id_usuario ?>";
+            var tempo = "<?php echo $tempo['timestamp'] ?>";
+
+            function tempo_sessao() {
+                var timeOut = new Date();
+                var hora = timeOut.toLocaleTimeString();
+                $("#tempo_sessao").html(hora);
+                if (tempoAlerta <= hora) {
+                    clearInterval(verificaSessao)
+                    let timerInterval
+                    Swal.fire({
+                        title: 'Sua sessão irá expirar em breve!',
+                        confirmButtonText: 'Atualizar',
+                        html: 'Restam <strong></strong><br/>',
+                        icon: "warning",
+                        allowOutsideClick: false,
+                        timer: 10 * 60 * 1000, // 10 minutos
+                        onOpen: () => {
+                            const content = Swal.getHtmlContainer()
+                            const $ = content.querySelector.bind(content)
+                            timerInterval = setInterval(() => {
+                                getSeconds = (Swal.getTimerLeft() / 1000).toFixed();
+                                minutes = Math.floor(getSeconds / 60);
+                                seconds = getSeconds - (minutes * 60);
+
+                                timeString = minutes.toString().padStart(2, '0') + ':' +
+                                    seconds.toString().padStart(2, '0');
+
+                                Swal.getHtmlContainer().querySelector('strong')
+                                    .textContent = timeString
+                            }, 1000)
+                        },
+                        onClose: () => {
+                            clearInterval(timerInterval)
+                        }
+                    }).then((result) => {
+                        if (result.isConfirmed) {
+                            $.ajax({
+                                type: "POST",
+                                url: "<?php echo base_url(); ?>/login/renovar_sessao",
+                                data: {
+                                    id: id,
+                                    timestamp: tempo
+                                },
+                                success: function(response) {
+                                    console.log(response)
+                                    if (response.type === 'success') {
+                                        formWarning(response);
+                                        window.location.reload(true);
+                                    } else {
+                                        formWarning(response)
+                                    }
+                                }
+                            });
+                        } else {
+                            timeoutSessao()
+                        }
+                    })
+                }
+                if (tempoSessao <= hora) {
+                    timeoutSessao()
+                    clearInterval(verificaSessao)
+                }
+            };
+
+            function timeoutSessao() {
+                $.ajax({
+                    type: "POST",
+                    url: "<?php echo base_url(); ?>/login/timeout_sessao",
+                    data: {
+                        id: id,
+                        id_usuario: idUser,
+                        timestamp: tempo
+                    },
+                    success: function(response) {
+                        console.log(response)
+                        if (response.type === 'error') {
+                            formWarning(response);
+                            localStorage.setItem("alertaSessao", true)
+                            window.location.replace('<?php echo base_url('login'); ?>');
+                        } else {
+                            formWarning(response)
+                        }
+                    },
+                });
+            };*/
+            
+            $('.renovarSessao').click(function(e) {
+                $.ajax({
+                    type: "POST",
+                    url: "<?php echo base_url(); ?>/login/renovar_sessao",
+                    data: {
+                        id: id,
+                        timestamp: tempo
+                    },
+                    success: function(response) {
+                        console.log(response)
+                        if (response.type === 'success') {
+                            formWarning(response);
+                            window.location.reload(true);
+                        } else {
+                            formWarning(response)
+                        }
+                    }
+                })
+            });
+        <?php endif; ?>
+
+
         <?php if (isset($this->session->id_fornecedor)) : ?>
             var urlCorreio = "<?php echo base_url('global/comunicacao/checkAllMessages'); ?>";
 
