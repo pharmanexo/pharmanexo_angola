03daf5616d8e2d4712839623ae030877daf271c3
Felipe Lima <felipe.lima@pharmanexo.com.br>
2022-12-16 12:35:04 -0300

    upgrade depara

--- application/views/fornecedor/cotacoes/find_product_sintese.php
+++ application/views/fornecedor/cotacoes/find_product_sintese.php
@@ -90,7 +90,7 @@
                 e.preventDefault();
 
                 var dados = [];
-                codprod = $(this).data('codproduto');
+                console.log('<?php echo $id_prod_cot; ?>');
 
                 $.map(table.rows('.selected').data(), function(item) {
 
@@ -99,7 +99,7 @@
                         cd_produto: item.codigo,
                         id_sintese: $('#data-table').data('sintese'),
                         id_cliente: $('#id_cliente').val(),
-                        id_produto_comprado: codprod //produto cotado
+                        id_produto_comprado: $('#id_prod_cot').val(), //produto cotado
                     });
                 });
 
