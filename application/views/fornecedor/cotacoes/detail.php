91ac0a355939f92000056a2dd19a510adda83bba
Felipe Lima <felipe.lima@pharmanexo.com.br>
2022-12-26 16:24:53 -0300

    delete DePara

--- application/views/fornecedor/cotacoes/detail.php
+++ application/views/fornecedor/cotacoes/detail.php
@@ -1282,14 +1282,16 @@
                     $(row).data('id', data.id_produto).css('cursor', 'pointer');
                 },
                 drawCallback: function() {
-
+                    removeRows(tabela, data_tabela);
                 }
             });
 
-            data_tabela.forEach(function(codigo) {
-                table.column(1).search(codigo).rows().remove().draw();
-            });
-            table.ajax.reload();
+            function removeRows(tabela, data_tabela) {
+                data_tabela.forEach(function(codigo) {
+                    table.column(1).search(codigo).rows().remove().draw();
+                });
+            }
+
 
             $('.btnCombinarUpgrade').on('click', function(e) {
                 e.preventDefault();
