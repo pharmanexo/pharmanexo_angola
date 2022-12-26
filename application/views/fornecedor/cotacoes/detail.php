c3fdc34aa512e80a36e607cc0ba4915ce9d06712
Felipe Lima <felipe.lima@pharmanexo.com.br>
2022-12-26 10:36:34 -0300

    delete DePara

--- application/views/fornecedor/cotacoes/detail.php
+++ application/views/fornecedor/cotacoes/detail.php
@@ -326,6 +326,7 @@
             $('.removerDePara').click(function() {
 
                 var container = $(this).parent().parent().parent().parent();
+                console.log(container);
                 var cod_prod = $(this).data('cod');
                 var cliente = $(this).data('cliente');
                 var sintese = $(this).data('sintese');
