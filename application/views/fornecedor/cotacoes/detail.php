ffae0b7a45b3206b183c01e43a2f67b51555858d
Felipe Lima <felipe.lima@pharmanexo.com.br>
2022-12-28 00:54:20 -0300

    botão modal upgrade depara

--- application/views/fornecedor/cotacoes/detail.php
+++ application/views/fornecedor/cotacoes/detail.php
@@ -230,6 +230,9 @@
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="historicoModalLabel"></h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
                 </div>
                 <div class="modal-body modalUpgradeDePara">
                     <div class="upgradeModal"></div>
