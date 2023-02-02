45f07a628e4886a86fc1d6eeecafca4d3c8ba7b3
Marlon Boecker <marlon.mbes@gmail.com>
2023-02-02 14:16:48 -0300

    update form conf envio

--- application/views/fornecedor/regras_vendas/configuracoes_envio/modal.php
+++ application/views/fornecedor/regras_vendas/configuracoes_envio/modal.php
@@ -57,6 +57,15 @@
                             </div>
                         </div>
                     </div>
+
+                    <div class="row mx-auto">
+                        <div class="col">
+                            <div class="form-group">
+                                <label for="observacao">Enviar validade na observação</label>
+                                <input type="checkbox" <?php if (isset($dados['validade']) && $dados['validade'] == 1) echo 'checked'; ?> value="1" name="validade" id="validade"> SIM
+                            </div>
+                        </div>
+                    </div>
                 </form>
             </div>
 
